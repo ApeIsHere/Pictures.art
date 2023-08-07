e7ff10b37f08dbbd4465944878f07dc69f88fd9c
ApeIsHere <vladimir.volkov.mg@gmail.com>
2023-08-07 17:11:22 +0300

    PicSize + Collapse

---
+++ src/js/modules/accordion.js
@@ -0,0 +1,38 @@
+const accordion = (titleSelector) => {
+    const btns = document.querySelectorAll(titleSelector);
+        //   blocks = document.querySelectorAll(blockSelector);
+
+    // blocks.forEach(block => {
+    //     block.classList.add('animated', 'fadeInDown');
+    // });
+
+    // btns.forEach(btn => {
+    //     btn.addEventListener('click', function() {
+    //         if (!this.classList.contains('active')) {
+    //             btns.forEach(btn => {
+    //                 btn.classList.remove('active', 'active-style');
+    //             });
+    //             this.classList.add('active', 'active-style');
+    //         }
+    //     });
+    // });
+
+
+
+    btns.forEach(btn => {
+        btn.addEventListener('click', function() {
+
+            this.classList.toggle('active-style');
+            this.nextElementSibling.classList.toggle('active-content');
+            
+            if (this.classList.contains('active-style')) {
+                this.nextElementSibling.style.maxHeight = this.nextElementSibling.scrollHeight + 80 + "px";
+
+            } else {
+                this.nextElementSibling.style.maxHeight = '0px';
+            }
+        });
+    });
+};
+
+export default accordion;
